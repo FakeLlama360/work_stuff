89fe4010550a954b71b46538ba204589e576b02a
FakeLlama360 <117266206+FakeLlama360@users.noreply.github.com>
2024-07-09 14:00:05 +0200

    trying to convert file

--- converter.py
+++ converter.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 
-xlms_file = 'Home(H:)/Samuel_Mlýnek/Programing/work_stuff/bulk-import_v5.1_cs.xlsm'
-df = pd.read_excel(xlms_file, engine='openpyxl')
+xlms_file = 'Work_stuff/import.xlsm'
+df = pd.read_excel(xlms_file)
 
-csv_file = 'Home(H:)/Samuel_Mlýnek/Programing/work_stuff'
+csv_file = 'Work_stuff/file.csv'
 df.to_csv(csv_file, index=False)
 
 print(f"File converted and saved to {csv_file}")
