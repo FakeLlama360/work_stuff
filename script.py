056b9f6428544211b1e2759f2ea44533b8fdd732
FakeLlama360 <117266206+FakeLlama360@users.noreply.github.com>
2024-07-10 11:24:47 +0200

    dfsdsdfs

--- script.py
+++ script.py
@@ -1,17 +1,23 @@
 import pandas as pd
 
+
 #Create a DataFrames
-data = {
-    'Name': ['Alice', 'Bob', 'Charlie'],
-    'Age': [25, 30, 35],
-    'City': ['New York', 'Los Angeles', 'Chicago']
+data ={
+    'number' :[1],
+    'stock' :[20],
+    'name' :['NSP-STICK'],
+    'id_num' :[23121],
+    'price' :[20],
+    'type' :['kč'],
+    'kategory' :['Electronicks']
 }
 
 df = pd.DataFrame(data)
-#
+
+#Specify the path for the CSV file
 csv_file = 'file.csv'
 
-#
+#Write the DataFrame o a CSV file
 df.to_csv(csv_file, index=False)
 
 print(f"Data written to {csv_file}")
